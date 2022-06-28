b702d88db988640835d5d3ee9800ebc1f663085e
jihunPark <ongamenet87@gmail.com>
2022-06-29 00:59:25 +0900

    220629-1.Variable

---
+++ src/com/java/lec1/Variable.java
@@ -0,0 +1,24 @@
+package com.java.lec1;
+import com.java.domain.Person;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Variable {
+
+    public static void main(String[] args) {
+        long number1 = 10L;
+        final long number2 = 10L;
+
+        Long number3 = 1_000L;
+        Person person = new Person("Jihun");
+
+
+        // final 컬렉션에 element 추가 가능
+        final List<Integer> numbers = new ArrayList<>(Arrays.asList(1,2));
+        numbers.add(3);  // 컬렉션에 값 추가 가능
+
+    }
+
+}
---
+++ src/com/kotlin/domain/Person.kt
@@ -0,0 +1,4 @@
+package com.kotlin.domain
+
+class Person {
+}
