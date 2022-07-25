09053785978bb42848614cb6cd963940c42040d1
Jihun Park <jihun.park@navercorp.com>
2022-07-26 08:24:23 +0900

    220726-refactoring2

--- src/main/java/Lec1_variable.java
+++ src/main/java/Lec1_variable.java
@@ -1,4 +1,4 @@
-import com.domain.Person;
+import domain.Person;
 
 import java.util.ArrayList;
 import java.util.Arrays;
---
+++ src/main/java/domain/Person.java
@@ -0,0 +1,22 @@
+package domain;
+
+/**
+ * Created by jihun.park@navercorp.com on 2022-07-26.
+ */
+public class Person {
+
+    private String name;
+
+    public Person(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
+
